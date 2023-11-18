c13e3381c00025e8fcec94dba362d6cf75d3c32c
Parth Sudan <parth7k4@gmail.com>
2023-11-18 12:40:11 +0530

    accepted

--- deja_vu.py
+++ deja_vu.py
@@ -5,12 +5,12 @@ for i in range(0, n):
     l3 = input().split()
     a2 = [int(j) for j in l2]
     a3 = [int(k) for k in l3]
-    m = 31
+    max = 31
     for q in a3:
-        if(m<=q): 
+        if(max<=q): 
             continue
         for j in range(0, len(a2)):
             if (a2[j]%(pow(2, q)))==0:
                 a2[j] += pow(2, (q-1))
-        m = q
+        max = q
     print(*a2)
